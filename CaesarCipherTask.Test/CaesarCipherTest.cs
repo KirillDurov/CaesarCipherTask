399f3f30cb051d1cc674475b38fbc49b1792c8dd
Kirill Durov <kirill.durow@gmail.com>
2022-02-19 23:10:18 +0200

    Caesar Cipher with UnitTest
    Caesar Cipher code with UnitTest written in C#

---
+++ CaesarCipherTask.Test/CaesarCipherTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CaesarCipherTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            string message = "Hello";
+            int key = 3;
+            string expected = "Khoor";
+
+            //act
+            CaesarCipher c = new CaesarCipher();
+            string actual = c.Encrypt(message, key);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ CaesarCipherTask/CaesarCipher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class CaesarCipher
+{
+    //symbols of the English alphabet
+    const string alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private string CodeEncode(string text, int k)
+    {
+        //add small letters to the alphabet
+        var fullAlfabet = alfabet + alfabet.ToLower();
+        var letterQty = fullAlfabet.Length;
+        var retVal = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            var c = text[i];
+            var index = fullAlfabet.IndexOf(c);
+            if (index < 0)
+            {
+                //if the symbol is not found, we add it as it is
+                retVal += c.ToString();
+            }
+            else
+            {
+                var codeIndex = (letterQty + index + k) % letterQty;
+                retVal += fullAlfabet[codeIndex];
+            }
+        }
+
+        return retVal;
+    }
+
+    //text encryption
+    public string Encrypt(string plainMessage, int key)
+        => CodeEncode(plainMessage, key);
+
+    //text decryption
+    public string Decrypt(string encryptedMessage, int key)
+        => CodeEncode(encryptedMessage, -key);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cipher = new CaesarCipher();
+        Console.Write("Enter Text: ");
+        var message = Console.ReadLine();
+        Console.Write("Enter Key: ");
+        var secretKey = Convert.ToInt32(Console.ReadLine());
+        var encryptedText = cipher.Encrypt(message, secretKey);
+        Console.WriteLine("Encrypted Message: {0}", encryptedText);
+        Console.WriteLine("Decrypted Message: {0}", cipher.Decrypt(encryptedText, secretKey));
+        Console.ReadLine();
+    }
+}
